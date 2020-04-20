cda5ddfc80c9c1d28620887cff580ff107f1890d
chayatornp <42747924+chayatornp@users.noreply.github.com>
2020-04-20 18:31:37 +0700

    First Commit

---
+++ mission-4/src/main/java/com/example/mission4/configuration/DealsRowMapper.java
@@ -0,0 +1,18 @@
+package com.example.mission4.configuration;
+
+import com.example.mission4.model.Deals;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DealsRowMapper implements RowMapper<Deals> {
+    @Override
+    public Deals mapRow(ResultSet rs, int rowNum) throws SQLException{
+        Deals deals = new Deals();
+        deals.setId(rs.getString("deals_ref_id"));
+        deals.setName(rs.getString("deals_name"));
+        deals.setOriginalPrice(rs.getString("deals_original_price"));
+        deals.setDiscountedPrice(rs.getString("deals_discounted_price"));
+        return deals;
+    }
+}
---
+++ mission-4/src/main/java/com/example/mission4/model/Deals.java
@@ -0,0 +1,34 @@
+package com.example.mission4.model;
+
+import lombok.Data;
+import java.util.UUID;
+
+public @Data class Deals {
+    public UUID id = UUID.randomUUID();
+    public String name;
+    public String originalPrice;
+    public String discountedPrice;
+
+    //GETTER & SETTER
+    public String getName() {
+        return name;
+    }
+    public String getOriginalPrice(){
+        return originalPrice;
+    }
+    public String getDiscountedPrice(){
+        return discountedPrice;
+    }
+    public void setId(String id) {
+        this.id = UUID.fromString(id);
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setOriginalPrice(String originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+    public void setDiscountedPrice(String discountedPrice) {
+        this.discountedPrice = discountedPrice;
+    }
+}
