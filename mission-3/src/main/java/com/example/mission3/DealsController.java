488214e12e34c0b0b63ed22a3d8f08f7a54f4e21
chayatornp <42747924+chayatornp@users.noreply.github.com>
2020-04-22 22:52:56 +0700

    Update DealsController.java

--- mission-3/src/main/java/com/example/mission3/DealsController.java
+++ mission-3/src/main/java/com/example/mission3/DealsController.java
@@ -3,7 +3,7 @@ package com.example.mission3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.UUID;
+
 @RestController
 public class DealsController {
     @Autowired
@@ -13,27 +13,24 @@ public class DealsController {
         return dealsService.getDeals();
     }
     @GetMapping("/deals/{id}")       //GET DEAL BY ID
-    public List<Deals> getDealById(@PathVariable String id) {
-        return dealsService.getDealById(id);
+    public List<Deals> getDealById(@PathVariable String id)  {
+            return dealsService.getDealById(id);
     }
     @PostMapping("/deals/")  //POST TO INSERT NEW DEAL
-    public List<Deals> createDeals(@RequestBody Deals deals) {
-        dealsService.createDeals(deals);
-        return dealsService.getDeals();
-    }
+    public void createDeals(@RequestBody Deals deals) {
+            dealsService.createDeals(deals);
+        }
     @PutMapping("/deals/{id}")  //PUT DEAL BY ID
-    public List<Deals> updateDeals(@RequestBody Deals deals, @PathVariable String id) {
+    public void updateDeals( @RequestBody Deals deals, @PathVariable String id){
         dealsService.updateDeals(deals, id);
-        return dealsService.getDeals();
     }
     @DeleteMapping("/deals/{id}") //DELETE DEAL BY ID
-    public Object deleteDealsById(@PathVariable String id) {
+    public void deleteDealsById( @PathVariable String id) {
         dealsService.deleteDealsById(id);
-        return dealsService.getDeals();
     }
     @DeleteMapping("/deals/")  //DELETE ALL DEALS
-    public Object deleteDeals() {
-        dealsService.deleteDeals();
+    public Object  deleteDeals() {
+         dealsService.deleteDeals();
         return dealsService.getEmptyMessage();
     }
 }
